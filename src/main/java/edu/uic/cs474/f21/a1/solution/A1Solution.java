4ea503052e8effd64be071f196d45376ec8c892d
Anupreet Paulkar <apaulk2@uic.edu>
2023-02-08 23:33:29 -0600

    test cas 1

--- src/main/java/edu/uic/cs474/f21/a1/solution/A1Solution.java
+++ src/main/java/edu/uic/cs474/f21/a1/solution/A1Solution.java
@@ -3,12 +3,19 @@ package edu.uic.cs474.f21.a1.solution;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import edu.uic.cs474.f21.a1.DynamicDispatchExplainer;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class A1Solution implements DynamicDispatchExplainer {
     @Override
     public Set<String> explain(Map<String, ClassOrInterfaceDeclaration> classes, String receiverType, String methodName, String... argumentTypes) {
-        throw new Error("NOt implemented");
+        Set<String> ret = new HashSet<>();
+
+        ClassOrInterfaceDeclaration d = classes.get(receiverType);
+        if(!d.getMethodsByName(methodName).isEmpty()){
+            ret.add(d.getNameAsString());
+        }
+        return ret;
     }
 }
