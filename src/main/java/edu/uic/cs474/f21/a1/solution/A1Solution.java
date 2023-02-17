9262ead3afc460aac331c060922ca4c840adf0bc
Anupreet Paulkar <apaulk2@uic.edu>
2023-02-16 21:44:22 -0600

    first commit

--- src/main/java/edu/uic/cs474/f21/a1/solution/A1Solution.java
+++ src/main/java/edu/uic/cs474/f21/a1/solution/A1Solution.java
@@ -1,11 +1,12 @@
 package edu.uic.cs474.f21.a1.solution;
 
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import edu.uic.cs474.f21.a1.DynamicDispatchExplainer;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+
 
 public class A1Solution implements DynamicDispatchExplainer {
     @Override
@@ -13,9 +14,33 @@ public class A1Solution implements DynamicDispatchExplainer {
         Set<String> ret = new HashSet<>();
 
         ClassOrInterfaceDeclaration d = classes.get(receiverType);
+
         if(!d.getMethodsByName(methodName).isEmpty()){
             ret.add(d.getNameAsString());
+            return ret;
         }
+
+        while (!d.isEmpty()) {
+            ClassOrInterfaceType a = d.getExtendedTypes().get(0);
+            d = classes.get(a.getName().asString());
+            if (!d.getMethodsByName(methodName).isEmpty()) {
+                ret.add(d.getNameAsString());
+                return ret;
+            }
+
+        }
+        for(ClassOrInterfaceType classOrInterfaceType : d.getExtendedTypes()) {
+            String name = classOrInterfaceType.getNameAsString();
+            if(d.getMethodsByName(methodName).isEmpty()) {
+                ret.addAll(explain(classes, name, methodName, argumentTypes));
+            }
+        }
+//        ClassOrInterfaceType b = d.getExtendedTypes();
+//        if (!d.getExtendedTypes().isEmpty()){
+//            for (ClassOrInterfaceType classOrInterfaceType: a) {
+//                explain(classes, d.getNameAsString(), methodName, argumentTypes);
+//            }
+//        }
         return ret;
     }
 }
