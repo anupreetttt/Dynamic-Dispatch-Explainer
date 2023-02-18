3911f51fd6a2ed268f02d1104949bf058097706c
Anupreet Paulkar <apaulk2@uic.edu>
2023-02-17 18:27:57 -0600

    Test6 testing

--- src/main/java/edu/uic/cs474/f21/a1/solution/A1Solution.java
+++ src/main/java/edu/uic/cs474/f21/a1/solution/A1Solution.java
@@ -17,34 +17,37 @@ public class A1Solution implements DynamicDispatchExplainer {
 
         if(!d.getMethodsBySignature(methodName, argumentTypes).isEmpty() && !d.getMethodsBySignature(methodName, argumentTypes).get(0).isAbstract()){
             ret.add(d.getNameAsString());
-            return ret;
+//            return ret;
         }
-        if(d.getExtendedTypes() == null) {
-            return ret;
-        }
+//        if(d.getExtendedTypes() == null) {
+//            return ret;
+//        }
 
         while (!d.getExtendedTypes().isEmpty()) {
             ClassOrInterfaceType a = d.getExtendedTypes().get(0);
             d = classes.get(a.getName().asString());
             if (!d.getMethodsBySignature(methodName, argumentTypes).isEmpty() && !d.getMethodsBySignature(methodName, argumentTypes).get(0).isAbstract() && !d.getMethodsBySignature(methodName, argumentTypes).get(0).isPrivate() && !d.getMethodsBySignature(methodName, argumentTypes).get(0).isStatic()) {
                 ret.add(d.getNameAsString());
-                return ret;
+                break;
             }
 
-//            for (MethodDeclaration method : d.getMethodsBySignature(methodName, argumentTypes)) {
-//                if (method.isAbstract()) {
-//                    // add the class to the set if the method is abstract
-//                    ret.add(d.getNameAsString());
-//                    return ret;
-//                }
-//                if (method.isPrivate() || method.isStatic()) {
-//                    // add the class to the set if the method is private or static
-//                    ret.add(d.getNameAsString());
-//                    return ret;
-//                }
+            if (d.getNameAsString().equals("Object")) {
+                System.out.println(d.getNameAsString());
+                ret.add("java.lang.Object");
+                break;
+            }
+//            else {
+//                d = classes.get("java.lang.Object");
+//                System.out.println(d.getNameAsString());
+//                ret.add("java.lang.Object");
 //            }
-        }
 
+            // Move up the class hierarchy
+//            if (!d.getExtendedTypes().isEmpty()) {
+//                ClassOrInterfaceType nextClass = d.getExtendedTypes().get(0);
+//                d = classes.get(nextClass.getNameAsString());
+
+        }
         return ret;
     }
 }
