2eb819d19a1177c249388583df1c7cda5c4f622b
Anupreet Paulkar <apaulk2@uic.edu>
2023-02-23 04:12:48 -0600

    Test 1 pass

--- src/main/java/edu/uic/cs474/s23/a2/solution/A2Solution.java
+++ src/main/java/edu/uic/cs474/s23/a2/solution/A2Solution.java
@@ -2,13 +2,29 @@ package edu.uic.cs474.s23.a2.solution;
 
 import edu.uic.cs474.s23.a2.ObjectInspector;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.Map;
 
 public class A2Solution implements ObjectInspector {
 
     @Override
     public Map<String, String> describeObject(Object o) {
-        return null;
+
+        Class<?> c = o.getClass();  // getting class of o
+        Field[] fs = c.getDeclaredFields(); // now we can get all the fields in c by declaring Field[] in String
+        Map<String, String> ret = new HashMap<>();
+        for (Field f : fs) {
+            try {
+                String key = f.getName();
+                Object value = f.get(0);
+                String setValue = (String) value;
+                ret.put(key, setValue);
+            } catch (ReflectiveOperationException e) {
+                throw new Error(e);
+            }
+        }
+        return ret;
     }
 
     @Override
