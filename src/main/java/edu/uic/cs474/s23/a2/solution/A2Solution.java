06ee8a2c77af9aa4249122e4741dd5f05ca971f3
Anupreet Paulkar <apaulk2@uic.edu>
2023-02-26 05:38:21 -0600

    Handling exception and passing 6th and 5th part 2

--- src/main/java/edu/uic/cs474/s23/a2/solution/A2Solution.java
+++ src/main/java/edu/uic/cs474/s23/a2/solution/A2Solution.java
@@ -3,6 +3,8 @@ package edu.uic.cs474.s23.a2.solution;
 import edu.uic.cs474.s23.a2.ObjectInspector;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,43 +12,119 @@ public class A2Solution implements ObjectInspector {
 
     @Override
     public Map<String, String> describeObject(Object o) {
+// key: field name, value: value of the field
         Map<String, String> ret = new HashMap<>();
-        Class<?> c = o.getClass();
-        while (c != null) {
-            Field[] fs = c.getDeclaredFields();
-            for (Field f : fs) {
-                if (!f.canAccess(o)) {
-                    f.setAccessible(true);
-                }
-                try {
-                    String key = f.getName();
-                    Object value = f.get(o);
-                    String setValue;
-                    if (value == null) {
-                        setValue = "null";
-                    } else if (value instanceof Long) {
-                        setValue = "Boxed " + value + "#L";
-                    } else if (value instanceof Float) {
-                        setValue = "Boxed " + value + "#F";
-                    } else if (value instanceof Double) {
-                        setValue = value + "#D";
-                    } else if (value instanceof Short) {
-                        setValue = "0" + Integer.toOctalString((Short) value);
-                    } else if (value instanceof Byte) {
-                        setValue = "0x" + String.format("%02x", (Byte) value);
-                    } else if (value instanceof Character) {
-                        setValue = "'" + value + "'";
-                    } else {
-                        setValue = value.toString();
+
+        try{
+            Class<?> c = (Class<?>) o;
+//            System.out.println(c.getSimpleName());
+            while (c != null) {
+                Field[] fields = c.getDeclaredFields();
+                for (Field field : fields) {
+                    field.setAccessible(true);
+                    String fieldName = field.getName();
+                    if (Modifier.isStatic(field.getModifiers())) {
+                        fieldName = c.getSimpleName() + "." + fieldName;
                     }
-                    ret.put(key, setValue);
-                } catch (IllegalAccessException e) {
-                    // Do nothing and move on to the next field
+                    try {
+                        Object getObjVal = field.get(o);
+                        if (getObjVal != null) {
+                            ret.put(fieldName, getObjVal.toString());
+                        } else {
+                            ret.put(fieldName, "null");
+                        }
+                    } catch (IllegalArgumentException e) {
+//                        throw new Error(e);
+                    }
                 }
+                c = c.getSuperclass();
             }
-            c = c.getSuperclass();
+            return ret;
+        } catch (Exception e) {
+            System.out.println("Test" + e);
+//                    throw new RuntimeException("Error describing object", e);
         }
+        try {
+            Class<?> c = o.getClass();  // getting class of o
+            while (c != null) {
+                Field[] fs = c.getDeclaredFields(); // now we can get all the fields in c by declaring Field[] in String
+                for (Field f : fs) {
+                    if (!f.canAccess(o)) {
+                        f.setAccessible(true);
+                    }
+                    try {
+                        String key = f.getName();
+                        Object value = f.get(o); // Returns the value of the field represented by this Field, on the specified object.
+//                    System.out.println(value);
+                        String setValue;
+                        if (value == null) {
+                            setValue = "null";
+                        } else if (value instanceof Long) {
+                            setValue = "Boxed " + value + "#L";
+                        } else if (value instanceof Float) {
+                            setValue = "Boxed " + value + "#F";
+                        } else if (value instanceof Double) {
+                            setValue = value + "#D";
+                        } else if (value instanceof Short) {
+                            setValue = "0" + Integer.toOctalString((Short) value);
+                        } else {
+                            setValue = value.toString();
+//                        System.out.println(setValue);
+                        }
+                        ret.put(key, setValue);
+//                    System.out.println(ret.keySet());
+//                    System.out.println(ret.entrySet());
+                    } catch (ReflectiveOperationException e) {
+                        throw new Error(e);
+                    }
+                }
+
+                c = c.getSuperclass();
+            }
+        } catch (Exception | Error e)  {
+
+        }
+        Class<?> c1 = o.getClass();
+
+        while(c1 != null) {
+            Field[] fs = c1.getDeclaredFields();
+            for (Field f : fs) {
+                try {
+                    f.setAccessible(true);
+                    Object value = f.get(o);
+
+                    if (value == null) {
+                        ret.put(f.getName(), "");
+                    } else {
+                        String setValue = "";
+                        try {
+                            setValue = value.toString();
+                        } catch (Throwable t) {
+                            System.out.println(t);
+                            if (t instanceof InvocationTargetException && t.getCause() != null) {
+                                t = t.getCause();
+                            }
+
+                            if (t instanceof RuntimeException) {
+                                setValue = "Thrown exception: " + t.getClass().getName();
+                            } else if (t instanceof Error) {
+                                setValue = "Raised error: " + t.getClass().getName();
+                            } else {
+                                setValue = "Unhandled throwable: " + t.getClass().getName();
+                            }
+                        }
+                        ret.put(f.getName(), setValue);
+                    }
+                } catch (Exception e) {
+//                    throw new AssertionError("Unexpected IllegalAccessException", e);
+                }
+                return ret;
+            }
+            c1 = c1.getSuperclass();
+        }
+
         return ret;
+
     }
 
     @Override
@@ -69,67 +147,37 @@ public class A2Solution implements ObjectInspector {
                         f.set(o, newValue);
                     } else {
                         // Attempt to convert newValue to the field's type
-                        Object convertedValue = UpdatePrimitives(newValue, f.getType());
+                        Object convertedValue = convertValue(newValue, f.getType());
                         f.set(o, convertedValue);
                     }
                 } catch (IllegalAccessException e) {
                     // Do nothing and move on to the next field
                 }
-//                for (Field f : fs) {
-//                    try {
-//                        f.setAccessible(true);
-//                        Object value = f.get(o);
-//
-//                        if (value == null) {
-//                            ret.put(f.getName(), "");
-//                        } else {
-//                            String setValue = "";
-//                            try {
-//                                setValue = value.toString();
-//                            } catch (Throwable t) {
-//                                if (t instanceof InvocationTargetException && t.getCause() != null) {
-//                                    t = t.getCause();
-//                                }
-//
-//                                if (t instanceof RuntimeException) {
-//                                    setValue = "Thrown exception: " + t.getClass().getName();
-//                                } else if (t instanceof Error) {
-//                                    setValue = "Raised error: " + t.getClass().getName();
-//                                } else {
-//                                    setValue = "Unhandled throwable: " + t.getClass().getName();
-//                                }
-//                            }
-//                            ret.put(f.getName(), setValue);
-//                        }
-//                    } catch (IllegalAccessException e) {
-//                        throw new AssertionError("Unexpected IllegalAccessException", e);
-//                    }
-//                }
             }
             c = c.getSuperclass();
         }
     }
 
-    private Object UpdatePrimitives(Object obj, Class<?> findClass) {
-        if (obj == null) {
+    private Object convertValue(Object value, Class<?> targetClass) {
+        if (value == null) {
             return null;
         }
-        if (findClass.isAssignableFrom(obj.getClass())) {
-            return obj;
+        if (targetClass.isAssignableFrom(value.getClass())) {
+            return value;
         }
-        if (findClass == boolean.class || findClass == Boolean.class) {
-            return Boolean.parseBoolean(obj.toString());
-        } else if (findClass == byte.class || findClass == Byte.class) {
-            return Byte.parseByte(obj.toString());
-        } else if (findClass == char.class || findClass == Character.class) {
-            return obj.toString().charAt(0);
-        } else if (findClass == double.class || findClass == Double.class) {
-            return Double.parseDouble(obj.toString());
-        } else if (findClass == float.class || findClass == Float.class) {
-            return Float.parseFloat(obj.toString());
-        } else if (findClass == int.class || findClass == Integer.class) {
-            return Integer.parseInt(obj.toString());
+        if (targetClass == boolean.class || targetClass == Boolean.class) {
+            return Boolean.parseBoolean(value.toString());
+        } else if (targetClass == byte.class || targetClass == Byte.class) {
+            return Byte.parseByte(value.toString());
+        } else if (targetClass == char.class || targetClass == Character.class) {
+            return value.toString().charAt(0);
+        } else if (targetClass == double.class || targetClass == Double.class) {
+            return Double.parseDouble(value.toString());
+        } else if (targetClass == float.class || targetClass == Float.class) {
+            return Float.parseFloat(value.toString());
+        } else if (targetClass == int.class || targetClass == Integer.class) {
+            return Integer.parseInt(value.toString());
         }
-        return obj;
+        return value;
     }
 }
